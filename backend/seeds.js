4299d6dddee9a7cceec59b05cfc99ef5a50be08c
Fahd Mohammed <fahdmoh.1@gmail.com>
2022-07-03 11:46:08 +0000

    completed database seeding task

--- backend/seeds.js
+++ backend/seeds.js
@@ -7,7 +7,10 @@ const Item = mongoose.model("Item");
 const Comment = mongoose.model("Comment");
 const User = mongoose.model("User");
 
-mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect(process.env.MONGODB_URI,{
+	useNewUrlParser: true,
+	useUnifiedTopology: true
+});
 
 const importData = async ()=>{
 	try{
